0d98e2d52b2287d50f44b0f4cd63533ae84e2b27
TomTheToad <tomthetoad951@gmail.com>
2019-05-21 00:12:38 -0400

    type: feat
    Checkpoint: Added Distance() method to route_model header file

--- src/route_model.cpp
+++ src/route_model.cpp
@@ -2,5 +2,13 @@
 #include <iostream>
 
 RouteModel::RouteModel(const std::vector<std::byte> &xml) : Model(xml) {
+    int counter = 0;
+    for (Model::Node node: this->Nodes()) {
+        m_Nodes.push_back(Node(counter, this, node));
+        counter++
+    }
 
+    float Distance(Node node) const {
+        return std::sqrt(std::pow((x - node.x), 2) + std::pow((y - node.y), 2));
+    }
 }
--- src/route_model.h
+++ src/route_model.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef ROUTE_MODEL_H
+#define ROUTE_MODEL_H
 
 #include <limits>
 #include <cmath>
@@ -12,9 +13,18 @@ class RouteModel : public Model {
     class Node : public Model::Node {
       public:
         // Add public Node variables and methods here.
+      	Node * parent = nullptr;
+      	float h_value = std::numeric_limits<float>::max();
+      	float g_value = 0.0;
+      	bool visited = false;
+      	std::vector<Node*> neighbors;
         
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
+      
+        float Distance(Node node) const {
+            return std::sqrt(std::pow((x - node.x), 2) + std::pow((y - node.y), 2));
+        }
       
       private:
         // Add private Node variables and methods here.
@@ -25,15 +35,18 @@ class RouteModel : public Model {
     // Add public RouteModel variables and methods here.
     RouteModel(const std::vector<std::byte> &xml);  
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
-    // Getter for m_Nodes
+  
+  	// Getters
+  	// Getter for m_Nodes
   	// Returns a reference
+  	// &vector<Node> SNodes() { return &m_Nodes; }
   	auto &SNodes() { return m_Nodes; }
-
 
   private:
     // Add private RouteModel variables and methods here.
-    // Private vector of nodes.
+	// Private vector of nodes.
   	// - Store all nodes from Open Street Map data
     std::vector<Node> m_Nodes;
-
 };
+
+#endif
